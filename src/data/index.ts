9495b268f4942cb08ddfbcef0f1fd80d130fffce
Alumno <Alumno@iMac-de-Administrador.local>
2025-07-23 11:53:09 -0600

    Funcion para eliminar la base de datos

---
+++ src/data/index.ts
@@ -0,0 +1,15 @@
+import { Sequelize } from 'sequelize-typescript';
+
+
+// hacer la conexion a base de datos
+const sequelize = new Sequelize();
+
+
+// funcion para limpiar la base de datos
+async function deleteDatabase() {
+    try {
+    } catch (error) {
+        
+    }
+    
+}
