85a56227541c78ad07caf2bcf0c42d16f04cf3b0
Liz <liz@Mac.lan>
2025-07-30 09:41:25 -0600

    Implementacion de swagger con usuarios

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -2,26 +2,33 @@ import swaggerJSDoc from "swagger-jsdoc";
 import { SwaggerUiOptions } from "swagger-ui-express";
 
 const options: swaggerJSDoc.Options = {
-    //todas las caraterisitcas que se le van a dar 
-    swaggerDefinition: {
-        openapi: '3.0.2',
-        tags: [{
-            name: 'Products',
-            description: 'Operaciones de API PERN con productos'
-        }],
-        info: {
-            title: 'REST API Node.js / Express / Typescript',
-            version: "1.0.0",
-            description: "API Documentacion para productos"
-        }
+  //todas las caraterisitcas que se le van a dar
+  swaggerDefinition: {
+    openapi: "3.0.2",
+    tags: [
+      {
+        name: "Products",
+        description: "Operaciones de API PERN con productos",
+      },
+      {
+        name: "Users",
+        description: "Operaciones de API PERN con usuarios",
+      },
+    ],
+
+    info: {
+      title: "REST API Node.js / Express / Typescript",
+      version: "1.0.0",
+      description: "API Documentacion para productos",
     },
-    apis: ['./src/router.ts']
-}
+  },
+  apis: ["./src/router.ts"],
+};
 
-const swaggerSpec = swaggerJSDoc(options)
+const swaggerSpec = swaggerJSDoc(options);
 
-const swaggerUiOptions : SwaggerUiOptions = {
-    customCss : `
+const swaggerUiOptions: SwaggerUiOptions = {
+  customCss: `
         .topbar-wrapper .link {
             content: url('https://codigoconjuan.com/wp-content/themes/cursosjuan/img/logo.svg');
             height: 80px;
@@ -31,11 +38,9 @@ const swaggerUiOptions : SwaggerUiOptions = {
             background-color: #2b3b45;
         }
     `,
-    customSiteTitle: 'Documentación REST API Express / TypeScript'
-}
+  customSiteTitle: "Documentación REST API Express / TypeScript",
+};
 
-export default swaggerSpec
+export default swaggerSpec;
 
-export {
-    swaggerUiOptions
-}
+export { swaggerUiOptions };
--- src/router.ts
+++ src/router.ts
@@ -1,17 +1,25 @@
-import { Router } from 'express';
+import { Router } from "express";
 import {
   createProduct,
   getProductId,
   getProducts,
   updateProduct,
   deleteProduct,
-  updateAvailability
-} from './handlers/product';
+  updateAvailability,
+} from "./handlers/product";
 
-import { handleInputErrors } from './middleware';
-import { body, param } from 'express-validator';
-import { methods } from './middleware/methods';
-import { eliminar } from './middleware/delete';
+import {
+  createUser,
+  getUsers,
+  getUserId,
+  updateUser,
+  deleteUser,
+} from "./handlers/user";
+
+import { handleInputErrors } from "./middleware";
+import { body, param } from "express-validator";
+import { methods } from "./middleware/methods";
+import { eliminar } from "./middleware/delete";
 
 const router = Router();
 /**
@@ -39,89 +47,144 @@ const router = Router();
  *                      example: true
  */
 
+/**
+ * @swagger
+ * components:
+ *      schemas:
+ *          Users:
+ *              type: object
+ *              properties:
+ *                  id:
+ *                      type: integer
+ *                      description: The User ID
+ *                      example: 1
+ *                  username:
+ *                      type: string
+ *                      description: The name of user
+ *                      example: Lizbeth
+ *                  email:
+ *                      type: string
+ *                      description: the email of user
+ *                      example: lizbeth@gmail.com
+ *                  password:
+ *                      type: enum
+ *                      description: user o admin
+ *                      example: user
+ *                  isActive:
+ *                      type: boolean
+ *                      description: The user could be active 
+ *                      example: true
+ */
 
 //hacer otro esquema de usuarios
 router.use(methods);
 
-router.get(
-  '/',
-  getProducts
-);
+router.get("/", getProducts);
 
 router.get(
-  '/:id',
-  param('id')
-    .isNumeric()
-    .withMessage('El id debe ser numerico'),
+  "/:id",
+  param("id").isNumeric().withMessage("El id debe ser numerico"),
   handleInputErrors,
   getProductId
 );
 
 router.post(
-  '/',
-  body('name')
-    .notEmpty()
-    .withMessage('El nombre es un campo requerido'),
+  "/",
+  body("name").notEmpty().withMessage("El nombre es un campo requerido"),
 
-  body('price')
+  body("price")
     .notEmpty()
-    .withMessage('El precio es un campo requerido')
+    .withMessage("El precio es un campo requerido")
     .isNumeric()
-    .withMessage('El dato no es numerico')
+    .withMessage("El dato no es numerico")
     .toFloat()
     .custom((value) => value > 0)
-    .withMessage('El precio debe ser mayor a 0'),
+    .withMessage("El precio debe ser mayor a 0"),
 
   handleInputErrors,
   createProduct
 );
 
 router.put(
-  '/:id',
-  param('id')
-    .isNumeric()
-    .withMessage('El id debe ser numerico'),
+  "/:id",
+  param("id").isNumeric().withMessage("El id debe ser numerico"),
 
-  body('name')
-    .notEmpty()
-    .withMessage('El nombre es un campo requerido'),
+  body("name").notEmpty().withMessage("El nombre es un campo requerido"),
 
-  body('price')
+  body("price")
     .notEmpty()
-    .withMessage('El precio es un campo requerido')
+    .withMessage("El precio es un campo requerido")
     .isNumeric()
-    .withMessage('El dato no es numerico')
+    .withMessage("El dato no es numerico")
     .toFloat()
     .custom((value) => value > 0)
-    .withMessage('El precio debe ser mayor a 0'),
+    .withMessage("El precio debe ser mayor a 0"),
 
-  body('availibility')
+  body("availibility")
     .isBoolean()
-    .withMessage('La disponibilidad debe ser booleana'),
+    .withMessage("La disponibilidad debe ser booleana"),
 
   handleInputErrors,
   updateProduct
 );
 
 router.patch(
-  '/:id',
-  param('id')
-    .isNumeric()
-    .withMessage('El id debe ser numerico'),
+  "/:id",
+  param("id").isNumeric().withMessage("El id debe ser numerico"),
 
   handleInputErrors,
   updateAvailability
 );
 
 router.delete(
-  '/:id',
-  param('id')
-    .isNumeric()
-    .withMessage('El id debe ser numérico'),
+  "/:id",
+  param("id").isNumeric().withMessage("El id debe ser numérico"),
 
   handleInputErrors,
   eliminar,
   deleteProduct
+);
+
+//Usuarios
+
+router.get(
+  "/user/:id",
+  param("id").isNumeric().withMessage("El id debe ser numerico"),
+  param("id").isInt().withMessage("El id no es valido"),
+  getUserId,
+  handleInputErrors
+);
+
+router.post(
+  "/user",
+  body("username").notEmpty().withMessage("El nombre es un campo requerido"),
+
+  body("password")
+    .notEmpty()
+    .withMessage("La constraseña es un campo requerido"),
+  body("email").notEmpty().isEmail().withMessage("El email no es valido"),
+
+  handleInputErrors,
+  createUser
+);
+
+router.put(
+  "/user/:id",
+  body("username").notEmpty().withMessage("El nombre es un campo requerido"),
+
+  body("email").notEmpty().isEmail().withMessage("El email no es válido"),
+
+  param("id").isNumeric().withMessage("El id debe ser numerico"),
+  updateUser,
+  handleInputErrors
+);
+
+router.delete(
+  "/user/:id",
+  param("id").isNumeric().withMessage("El id debe ser numerico"),
+  param("id").isInt().withMessage("El id no es valido"),
+  deleteUser,
+  handleInputErrors
 );
 
 export default router;
